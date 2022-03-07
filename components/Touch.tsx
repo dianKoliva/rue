f6b7bd6b16afdb5d0566aeefd1c0b45f6fdaaf07
dianKoliva <dianpretty01@gmail.com>
2022-03-07 18:18:33 +0200

    new changes

--- components/Touch.tsx
+++ components/Touch.tsx
@@ -6,16 +6,67 @@ const Touch:NextPage = () => {
   return (
     <div>
 
-<div className={`w-full ${styles.tcont}`}>
+<div className={`w-full ${styles.tcont} `}>
+
+    
     <p className='text-white text-xl font-bold text-center pt-8 '>Get in touch with us</p>
 
-    <div className='flex'>
+    <div className='flex mt-10 p-4 pb-10'>
 
-        <div className='bg-white  h-96 w-96' >
+        <div className='bg-white p-6  ml-60  shadow-xl rounded-md   h-80 w-80' >
+<p className='text-sm font-bold text-center'>Contact Details</p>
+
+<div className='mb-5 text-xs mt-1 text-center'>
+    <p className=''>788-888-888</p>
+    <p className=''>Kimironko kk49 </p>
+</div>
+
+
+<div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510403.19426079595!2d29.778611332812485!3d-1.9427570000000047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca5d13cdec6db%3A0x3979031864bbd4eb!2sCar%20Wash!5e0!3m2!1ssw!2srw!4v1646669031998!5m2!1ssw!2srw" width="270" height="200" style={{border:"0"}} 
+ aria-hidden="false"
+
+loading="lazy"></iframe>
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+        <div className='bg-white p-6 absolute right-0 mr-60  shadow-xl rounded-md   h-80 w-80' >
 <p className='font-bold text-center'>Message Us</p>
 
+
+<div className='pl-4 pt-6'>
+
+<div className='text-sm mt-4'>
 <p>Email</p>
-<input type="text" name="" id="" />
+<input  className=' w-60 border-b-2 border-solid border-black
+focus:outline-none
+' type="text" name="" id="" />
+</div>
+<div className='text-sm mt-6'>
+<p>Message</p>
+<input  className='  w-60  border-b-2 border-solid border-black
+focus:outline-none
+' type="text" name="" id="" />
+</div>
+
+</div>
+
+<div className='mt-14 text-center'>
+<button className='bg-black rounded-sm  text-white text-sm px-6 py-2 '>
+    send message
+</button>
+</div>
+
+
+
 
         </div>
 
