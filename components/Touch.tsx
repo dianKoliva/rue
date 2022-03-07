01e21afb1acbdccc8376e4b1da252293feb9a2b3
dianKoliva <dianpretty01@gmail.com>
2022-03-07 14:38:39 +0200

    new changes

--- components/Touch.tsx
+++ components/Touch.tsx
@@ -7,7 +7,19 @@ const Touch:NextPage = () => {
     <div>
 
 <div className={`w-full ${styles.tcont}`}>
-    <p>Get in touch with us</p>
+    <p className='text-white text-xl font-bold text-center pt-8 '>Get in touch with us</p>
+
+    <div className='flex'>
+
+        <div className='bg-white  h-96 w-96' >
+<p className='font-bold text-center'>Message Us</p>
+
+<p>Email</p>
+<input type="text" name="" id="" />
+
+        </div>
+
+    </div>
 </div>
 
     </div>
